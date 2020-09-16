12029b58be92e89f164b7743def09b20317e3010
RafaelRNG <rafael.gomila@hotmail.com>
2020-09-16 04:27:20 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
@@ -0,0 +1,62 @@
+package com.devsuperior.dspesquisa.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_genre")
+public class Genre implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "genre")
+    private List<Game> games = new ArrayList<Game>();
+
+    public Genre(){}
+
+    public Genre(Long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId(){
+        return this.id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Game> getGames(){
+        return this.games;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Genre genre = (Genre) o;
+        return id.equals(genre.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
