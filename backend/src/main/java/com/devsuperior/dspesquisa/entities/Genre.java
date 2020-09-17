ef90a8f52ae188a1489c6c947e4861f725c3f274
RafaelRNG <rafael.gomila@hotmail.com>
2020-09-17 01:00:31 -0300

    DTO, Services

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dspesquisa.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.ArrayList;
