9c710be4b6887d52efd32eac257cff599c65341c
RafaelRNG <rafael.gomila@hotmail.com>
2020-09-17 04:44:06 -0300

    POST records

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/resources/RecordResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dspesquisa.resources;
+
+import com.devsuperior.dspesquisa.dto.RecordDTO;
+import com.devsuperior.dspesquisa.dto.RecordInsertDTO;
+import com.devsuperior.dspesquisa.entities.Record;
+import com.devsuperior.dspesquisa.services.RecordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/records")
+public class RecordResource {
+
+    @Autowired
+    private RecordService service;
+
+    @PostMapping
+    public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto){
+        RecordDTO newDTO = service.insert(dto);
+        return ResponseEntity.ok().body(newDTO);
+    }
+}
